dc25492bec20f48524fe95fb3af4c5145a4da9c6
Denys <go4denys@gmail.com>
2023-08-24 11:18:12 -0400

    change branch

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -14,10 +14,10 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
 
 let notes = [
   {
@@ -37,10 +37,9 @@ let notes = [
   }
 ]
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (req, res) => {
   res.json(notes)
@@ -72,6 +71,11 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note)
 
   response.json(note)
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+  response.json(body)
 })
 
 app.get('/api/notes/:id', (request, response) => {
---
+++ part3/notes/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://notes_db:${password}@notedatabase.8bajkci.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+// set of rules or a blueprint
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// Model so-called 'constructor functions'
+const Note = mongoose.model('Note', noteSchema)
+
+// // the application creates a new note object with the help of the Note model
+// const note = new Note({
+//   content: "Micho is a cutie cat",
+//   important: true,
+// })
+
+// note.save().then(result => {
+//     console.log(result);
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// // retrieved all data from the database
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//         console.log(note);
+//     })
+//     mongoose.connection.close()
+// })
+
+// //  only include important notes
+// Note.find({ important: true }).then(result => {
+//     result.forEach(note => {
+//         console.log(note);
+//     })
+//     mongoose.connection.close()
+// })
