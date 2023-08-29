d473dd0d48c36cef5c4b820cd89570e39e7e2d86
Denys <go4denys@gmail.com>
2023-08-29 13:17:07 -0400

    finished exercise 4.5, test function finds out which blog has most likes

--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -15,8 +15,29 @@ const totalLikes = (blogs) => {
     }
     return sum
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.lenght === 0) {
+        return null
+    }
+    let favorite = blogs[0]
+
+    for (const blog of blogs)
+    {
+        if (blog.likes > favorite.likes) {
+            favorite = blog
+        }
+    }
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
   
   module.exports = {
       dummy,
-      totalLikes
+      totalLikes,
+      favoriteBlog
   }
